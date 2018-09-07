a6eb4522205f4d2b140fb7e2ec3b5f764cad95be
Agnieszka Herod <metalvege@gmail.com>
2018-09-07 10:04:02 +0100

    images added, styles edited

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const namesContainerStyle = {
     justifyContent: "space-around",
     alignItems: "center",
     height: "480px",
-    width: "600px",
+    width: "1000px",
     flexWrap: "wrap"
 }
 const hint = {
@@ -137,10 +137,12 @@ class NamesPage extends React.Component {
                     <button className="btnAdd">Add</button>
                 </form>
                 <NamesConatiner add={this.props.add1} names={this.state.names}/>
-                    <p className="girlPic"></p>
+
                 <Link to="/chorelist">
                     <Next/>
                 </Link>
+                <div className="girlPic"></div>
+
             </div>
         );
     }
@@ -237,6 +239,7 @@ class ChoresListPage extends React.Component {
                 <Link to="/table">
                     <Next/>
                 </Link>
+                <div className="utenPic"></div>
             </div>
         );
     }
@@ -345,36 +348,117 @@ class ChoresTablePage extends React.Component {
                 <PDFExport ref={(component) => this.pdfExportComponent = component} paperSize="A4" landscape={true}>
                     <TableChores cellChores={this.props.add4} cellNames ={this.props.add2}/>
                 </PDFExport>
-                    <button onClick={this.exportPDFWithComponent} className="btn">DOWNLOAD</button>
+                    <button onClick={this.exportPDFWithComponent} className="btnDownLoad btn grow">DOWNLOAD</button>
 
                 </div>
                 <Link to="final">
                     <Next/>
                 </Link>
+                <div className="tools"></div>
+
             </div>
         )
     }
 }
 
 class FinalPage extends React.Component {
-    render() {
-        return (
-            <div style={mainPageStyle}>
-                <Link to="/table">
-                    <Previous/>
-                </Link>
-                <div className="infoMssg">
-                    <h1>Your plan is ready!</h1>
-                </div>
-                <div style={{display: "flex"}} className="btnContainer">
-                    <button className="btnF">Download</button>
-                    <button className="btnF">Send By Mail</button>
-                </div>
-            </div>
-        )
-    }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameValidation: "",
+            emailValidation: "",
+            mssgValidation: "",
+            name: "",
+            email: "",
+            mssg: "",
+            select: "",
+            shown: true,
+            type: '',
+            flex: {flexDirection: "row"}
 
+        }
+    }
+
+
+    showForm = (event) => {
+        event.preventDefault();
+        this.setState({
+            shown: !this.state.shown
+
+        });
+    }
+    handleNameChange = (event) => {
+        this.setState({name: event.target.value});
+    };
+    handleEmailChange = (event) => {
+        this.setState({email: event.target.value});
+    };
+    handleMssgChange = (event) => {
+        this.setState({mssg: event.target.value});
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({nameValidation: event.target.value});
+        this.setState({emailValidation: event.target.value});
+        this.setState({mssgValidation: event.target.value});
+        console.log("formularz przesłany!");
+
+        if (this.state.name === "") {
+            console.log("name sprawdzony");
+            this.setState({nameValidation: "Write your name"})
+        }
+        if (!this.state.email.includes("@")) {
+            console.log("email sprawdzony");
+            this.setState({emailValidation: "Addressee's email must be set and valid"})
+        }
+        if (this.state.mssg === "") {
+            console.log("mssg sprawdzony");
+            this.setState({mssgValidation: "Message box cannot be empty"})
+        }
+        if(!(this.state.name === "") && this.state.email.includes("@") && !(this.state.mssg === "") ){
+            console.log("name git");
+
+        }
+    }
+        render()
+        {
+            let shown = {
+                display: this.state.shown ? "block" : "none"
+            };
+
+            let hidden = {
+                display: this.state.shown ? "none" : "block"
+            };
+            return (
+                <div style={mainPageStyle}>
+                    <Link to="/table">
+                        <Previous/>
+                    </Link>
+                    <div className="infoMssg">
+                        <h1>Your plan is ready!</h1>
+                    </div>
+                    <div className="btnContainer" style = {this.state.flex}>
+                        <button className="btnF">Download</button>
+                        <button className="btnF" onClick={this.showForm}>Send By Mail</button>
+
+                    </div>
+                    <div style={hidden}>
+
+                        <form onSubmit={this.handleSubmit} noValidate className="formFinal">
+                            <span className="validation">{this.state.nameValidation}</span>
+                            <input  className="formName" value={this.state.name} onChange={this.handleNameChange} placeholder="Your name"/>
+                            <span className="validation">{this.state.emailValidation}</span>
+                            <input  className="formEmail" type ="email" value={this.state.email} onChange={this.handleEmailChange} placeholder="Addressee's email"/>
+                            <span className="validation">{this.state.mssgValidation} </span>
+                            <textarea  className="formMssg" value={this.state.mssg} onChange={this.handleMssgChange}  placeholder="Your message"></textarea>
+                            <input  type="submit" className="btn"/>
+                        </form>
+                    </div>
+                </div>
+            )
+        }
+
+}
 class NotFound extends React.Component {
     render() {
         return (
